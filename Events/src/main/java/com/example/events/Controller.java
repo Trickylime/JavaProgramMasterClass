e9f00c21698ab8075a58da55409db169300c0f11
Trickylime <jackmcgeown@yahoo.co.uk>
2024-11-01 06:17:22 +0000

    Events && Event Handlers

---
+++ Events/src/main/java/com/example/events/Controller.java
@@ -0,0 +1,14 @@
+package com.example.events;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    public void onButtonClicked() {
+        System.out.println("Hello, " + nameField.getText());
+    }
+}
